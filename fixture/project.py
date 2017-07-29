06d87cf3f1ff66dd562e42ca8e9a5bbc385ee56b
VictorKazankov <sadovnichi@mail.ru>
2017-07-29 15:39:59 +0300

    Тест для удаления проекта

--- fixture/project.py
+++ fixture/project.py
@@ -54,3 +54,33 @@ class ProjectHelper:
             wd.find_element_by_name(field_name).click()
             wd.find_element_by_name(field_name).clear()
             wd.find_element_by_name(field_name).send_keys(text)
+
+    def count(self):
+        wd = self.app.wd
+        self.open_project_page()
+        return len(wd.find_elements_by_css_selector('.row-1 td a,.row-2 td a'))
+
+    def delete_project_by_id(self, id):
+        wd = self.app.wd
+        self.open_project_page()
+        self.select_project_by_id(id)
+        wd.find_element_by_css_selector("input[value='Delete Project']").click()
+        a = wd.current_url
+        if wd.current_url == 'http://localhost/mantisbt-1.2.19/manage_proj_delete.php':
+            wd.find_element_by_css_selector("input[value='Delete Project']").click()
+        else:
+            print("This is no delete page")
+
+
+
+    def select_project_by_id(self, id):
+        wd = self.app.wd
+        path_href = "http://localhost/mantisbt-1.2.19/manage_proj_edit_page.php?project_id="
+        for element in wd.find_elements_by_css_selector('.row-1 td a,.row-2 td a'):
+            name = element.text
+            href = element.get_attribute("href")
+            href.startswith("http://localhost/mantisbt-1.2.19/manage_proj_edit_page.php?project_id=")
+            id_css = href[70:]
+            if id == id_css:
+                wd.find_element_by_link_text("%s" % name).click()
+                break
